0d796f383ed418c1a96c95d6edf15899c69ffdb0
alber <alber@DESKTOP-VDCH9K4>
2020-03-05 23:30:50 +0100

    Eliminacion del contexto de anotaciones, innecesario para el ejemplo

--- spring/src/main/java/com/albertsalud/spring/annotations/HelloWorldPrinter.java
+++ spring/src/main/java/com/albertsalud/spring/annotations/HelloWorldPrinter.java
@@ -1,10 +1,12 @@
 package com.albertsalud.spring.annotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class HelloWorldPrinter {
 	
 	@Autowired
+	@Qualifier("helloWorld")
 	private HelloWorld helloWorld;
 	
 	public void setHelloWorld(HelloWorld helloWorld) {
